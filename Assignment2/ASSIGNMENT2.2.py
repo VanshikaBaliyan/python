c85614fcf15ddf8e1354cb5a1253d313f5047b2a
VanshikaBaliyan <vanshikabaliyan038@gmail.com>
2025-04-24 22:03:18 +0530

    assignment2
    assignment 2

---
+++ Assignment2/ASSIGNMENT2.2.py
@@ -0,0 +1,7 @@
+# Initializing the variable
+total_sum = 0
+
+# Use a for loop to add numbers from 1 to 50
+for number in range(1, 51):
+    total_sum += number
+print(f"The sum of numbers from 1 to 50 is: {total_sum}")
