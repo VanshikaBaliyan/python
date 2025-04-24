ed8f8c348e78db3edd8407fff75179a179bd342a
VanshikaBaliyan <vanshikabaliyan038@gmail.com>
2025-04-24 21:50:12 +0530

    task1
    python

---
+++ PYTHON/ASSIGNMENT1.2.py
@@ -0,0 +1,9 @@
+#user's first name
+first_name = input("Enter your first name: ")
+
+#user's last name
+last_name = input("Enter your last name: ")
+
+full_name = first_name + " " + last_name
+
+print(f"Hello, {full_name}! Welcome to the Python program.")
