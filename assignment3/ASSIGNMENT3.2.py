ce3d59291db86637228be77fbd0a84d8141f901c
VanshikaBaliyan <vanshikabaliyan038@gmail.com>
2025-04-24 22:12:45 +0530

    assignment3
    assignment3

---
+++ assignment3/ASSIGNMENT3.2.py
@@ -0,0 +1,13 @@
+
+import math
+
+number = float(input("Enter a number: "))
+
+# Performing calculations
+square_root = math.sqrt(number)
+log_value = math.log(number)
+sine_value = math.sin(number)
+
+print(f"Square root: {square_root}")
+print(f"Logarithm: {log_value}")
+print(f"Sine: {sine_value}")
